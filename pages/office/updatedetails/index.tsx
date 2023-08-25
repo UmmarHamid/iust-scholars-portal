495cbc0b6d251f4e94943321f940ec04a3770119
Ummar Hamid <ummar@192.168.1.4>
2023-08-25 10:00:01 +0530

    refactor

--- pages/office/updatedetails/index.tsx
+++ pages/office/updatedetails/index.tsx
@@ -3,26 +3,28 @@ import { MdArrowBackIosNew } from 'react-icons/md';
 import { BiLogOut } from 'react-icons/bi';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Box, Button, Container, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, SimpleGrid } from '@chakra-ui/react';
 import { RxUpdate } from 'react-icons/rx';
 import { HiDocumentArrowUp } from 'react-icons/hi2';
-const StyledIcon = styled.h1`
-  font-size: 46px;
-  font-weight: 700;
+const StyledIcon = styled.div`
+  font-size: 7em;
   color: #0c2b50;
-  padding-left: 30%;
 `;
 const StyledFooter = styled.footer`
-  left: 0;
   bottom: 0;
-  position: relative;
+  position: fixed;
   width: 100%;
   background-color: #002147;
   color: #fff;
   text-align: center;
   line-height: 50px;
 `;
-const LinkStyled = styled(Link)``;
+const LinkStyled = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 export const index = () => {
   return (
     <>
@@ -36,29 +38,34 @@ export const index = () => {
         height={'100px'}
       >
         <Link href='/office'>
-          <StyledIcon>{<MdArrowBackIosNew />}</StyledIcon>
+          <StyledIcon>{<MdArrowBackIosNew size={36} />}</StyledIcon>
         </Link>
-        <Heading as={'h2'} color={'teal'} paddingTop={'2%'} fontWeight={300}>
+        <Heading as={'h2'} color={'teal'} fontWeight={300}>
           Update Details
         </Heading>
         <Button colorScheme='red' leftIcon={<BiLogOut />}>
           Logout
         </Button>
       </Box>
-      <Box margin={'5%'} display={'flex'} justifyContent={'space-around'}>
-        <LinkStyled href='/'>
+      <SimpleGrid
+        columns={2}
+        gap={'100px'}
+        height={'calc(100vh - 150px)'}
+        padding={'5%'}
+      >
+        <LinkStyled href='/editscholar'>
           <StyledIcon> {<RxUpdate />} </StyledIcon>
           <Heading as={'h2'} color={'teal'} paddingTop={'2%'} fontWeight={300}>
             Edit Scholar
           </Heading>
         </LinkStyled>
-        <LinkStyled href='/'>
+        <LinkStyled href='/updateaward'>
           <StyledIcon> {<HiDocumentArrowUp />} </StyledIcon>
           <Heading as={'h2'} color={'teal'} paddingTop={'2%'} fontWeight={300}>
             Update Award
           </Heading>
         </LinkStyled>
-      </Box>
+      </SimpleGrid>
       <StyledFooter>
         © 2023 - Islamic University of Science and Technology.
       </StyledFooter>
