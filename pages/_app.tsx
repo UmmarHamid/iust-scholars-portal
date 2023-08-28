3ad3349d57e93b964bbdc92ecd53e5965c1952ea
Ummar Hamid <ummar@192.168.1.5>
2023-08-28 13:04:57 +0530

    add sign in functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { Database } from '../types/supabase';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const supabaseClient = createPagesBrowserClient<Database>();
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionContextProvider>
   );
 }
