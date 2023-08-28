ceefd2ce0dbb75f3337760b9180f23b48643a8a8
zubairahad <69452881+zubairahad@users.noreply.github.com>
2023-08-28 17:38:33 +0530

    add research-achievement-by-soet

---
+++ pages/researchachievementsoet/index.tsx
@@ -0,0 +1,172 @@
+import styled from 'styled-components';
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { BiArrowBack, BiLogOut } from 'react-icons/bi';
+import Head from 'next/head';
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tr,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+
+const StyledFooter = styled.footer`
+  bottom: 0;
+  position: fixed;
+  width: 100%;
+  background-color: #002147;
+  color: #fff;
+  text-align: center;
+  line-height: 50px;
+`;
+
+export const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>SOET</title>
+      </Head>
+
+      <Box
+        display={'flex'}
+        justifyContent={'space-around'}
+        alignItems={'center'}
+        height={'100px'}
+        marginLeft={'10%'}
+        marginRight={'10%'}
+      >
+        <Link href='/'>
+          <Button
+            leftIcon={<BiArrowBack />}
+            colorScheme='facebook'
+            variant='solid'
+          >
+            Back
+          </Button>
+        </Link>
+        <Heading as={'h2'} color={'teal'} fontWeight={500}>
+          Research Achievements By SOET
+        </Heading>
+      </Box>
+      <TableContainer display={'block'} maxWidth={'100%'} overflowX={'auto'}>
+        <Table>
+          <Tbody>
+            <Tr>
+              <Td isNumeric>1</Td>
+              <Td fontSize={'26px'}> Department of Civil Engineering</Td>{' '}
+              <Td color={'blue'}>
+                {' '}
+                <Link href='/'>Publications </Link>
+              </Td>{' '}
+              <Td color={'blue'}>
+                {' '}
+                <Link href='/'>Research Projects</Link>
+              </Td>
+              <Td color={'blue'}>
+                {' '}
+                <Link href='/'>Collaborations </Link>{' '}
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Dissertations </Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='https://scholar.google.com/citations?user=1OFoUYgAAAAJ&hl=en&authuser=1'>
+                  Google Scholars
+                </Link>
+              </Td>
+            </Tr>
+            <br />
+
+            <Tr>
+              <Td isNumeric>2</Td>
+              <Td fontSize={'26px'}> Department of Electrical Engineering</Td>
+              <Td color={'blue'}>
+                <Link href='/'>Thrust areas</Link>
+              </Td>{' '}
+              <Td color={'blue'}>
+                <Link href='/'>Publications</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Research Projects</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='https://scholar.google.com/citations?hl=en&user=tQLtj4AAAAAJ'>
+                  Google Scholars
+                </Link>
+              </Td>
+            </Tr>
+            <br />
+
+            <Tr>
+              <Td isNumeric>3</Td>
+              <Td fontSize={'24px'}> Department of Mechanical Engineering</Td>
+              <Td color={'blue'}>
+                <Link href='/'>Thrust areas</Link>
+              </Td>{' '}
+              <Td color={'blue'}>
+                <Link href='/'>Publications</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Journals</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Books</Link>
+              </Td>
+            </Tr>
+            <br />
+
+            <Tr>
+              <Td isNumeric>4</Td>
+              <Td fontSize={'24px'}> Department of Computer Science</Td>
+              <Td color={'blue'}>
+                <Link href='/'>Thrust areas</Link>
+              </Td>{' '}
+              <Td color={'blue'}>
+                <Link href='/'>Publications</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Journals</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Research Projects</Link>
+              </Td>
+            </Tr>
+            <br />
+            <Tr>
+              <Td isNumeric>5</Td>
+              <Td fontSize={'26px'}>
+                Department of Electronics <br />
+                <br />
+                and Communication Engineering
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Thrust areas</Link>
+              </Td>{' '}
+              <Td color={'blue'}>
+                <Link href='/'>Publications</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/'>Research Projects</Link>
+              </Td>
+              <Td color={'blue'}>
+                <Link href='/https://scholar.google.com/citations?hl=en&user=mDiLTIMAAAAJ'>
+                  Google Scholar
+                </Link>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <StyledFooter>
+        © 2023 - Islamic University of Science and Technology.
+      </StyledFooter>
+    </>
+  );
+};
+export default Index;
