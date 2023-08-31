5a41a7c26dc60284293aa7a402bfb506936f68ee
Ummar Hamid <ummar@192.168.1.7>
2023-08-31 14:58:37 +0530

    add srac member from supabase

--- pages/protected/office/sracmembers/index.tsx
+++ pages/protected/office/sracmembers/index.tsx
@@ -15,6 +15,8 @@ import {
   Tr,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { GetServerSideProps } from 'next';
+import supabase from '@/utils/supabase';
 const StyledIcon = styled.h1`
   font-size: 46px;
   font-weight: 700;
@@ -35,7 +37,14 @@ const ImageStyled = styled(Image)`
   padding: 10px;
   border-radius: 16px;
 `;
-export const index = () => {
+
+type sracMember = {
+  designation: string | null;
+  email: string | null;
+  id: string;
+  name: string | null;
+};
+export const index = ({ members }: any) => {
   return (
     <>
       <Head>
@@ -64,57 +73,26 @@ export const index = () => {
           variant='simple'
         >
           <Tbody>
-            <Tr>
-              <Td isNumeric>1</Td>
-              <Td>
-                <ImageStyled
-                  alt='Not available yet'
-                  width={100}
-                  height={75}
-                  src='/r.jfif'
-                />
-              </Td>
-              <Td>
-                <Heading>Not available yet</Heading>
-                <Text></Text>
-                <Text></Text>
-              </Td>
-              <Td color='teal'>
-                <Link href=''>Not available yet</Link>
-              </Td>
-              <Td color='teal'>
-                <Link href=''>Not available yet</Link>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td isNumeric>2</Td>
-              <Td>
-                <ImageStyled
-                  alt='Not available yet'
-                  width={100}
-                  height={75}
-                  src='/r.jfif'
-                />
-              </Td>
-              <Td>
-                <Heading>Not available yet</Heading>
-                <Text></Text>
-                <Text></Text>
-              </Td>
-              <Td color='teal'>
-                <Link href=''>
-                  <Text>Not available yet </Text>
-                </Link>
-              </Td>
-              <Td color='teal'>
-                <Link href=''>
-                  <Text>Not available yet</Text>
-                </Link>
-              </Td>
-              <Td color='teal'>
-                <Link href=''>Not available yet</Link>
-              </Td>
-            </Tr>
+            {members?.map((member: sracMember, index: number) => (
+              <Tr key={member.id}>
+                <Td isNumeric>{index + 1}</Td>
+                <Td>
+                  <ImageStyled
+                    alt='Not available yet'
+                    width={100}
+                    height={75}
+                    src='/r.jfif'
+                  />
+                </Td>
+                <Td>
+                  <Heading>{member.name}</Heading>
+                  <Text></Text>
+                  <Text></Text>
+                </Td>
+                <Td color='teal'>{member.designation}</Td>
+                <Td color='teal'>{member.email}</Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
@@ -125,3 +103,9 @@ export const index = () => {
   );
 };
 export default index;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data: members, error } = await supabase
+    .from('srac_profiles')
+    .select('*');
+  return { props: { members, error } };
+};
