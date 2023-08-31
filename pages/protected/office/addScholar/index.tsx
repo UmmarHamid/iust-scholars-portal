f21b0f8987966ac00aee5d85eab17b8bc8ce3e13
Ummar Hamid <ummar@192.168.1.7>
2023-08-31 15:31:02 +0530

    implement add scholar functionality

--- pages/protected/office/addScholar/index.tsx
+++ pages/protected/office/addScholar/index.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
+
 import { MdArrowBackIosNew } from 'react-icons/md';
 import {
   Box,
   Button,
-  Center,
   Container,
   FormControl,
-  FormHelperText,
   FormLabel,
   Heading,
   Input,
@@ -14,13 +13,44 @@ import {
 import { BiLogOut } from 'react-icons/bi';
 import Head from 'next/head';
 import Link from 'next/link';
-const StyledHeading = styled.div``;
+import { useState } from 'react';
+import supabase from '@/utils/supabase';
+import { useRouter } from 'next/router';
 const StyledIcon = styled.h1`
   font-size: 42px;
   font-weight: 700;
   color: #0c2b50;
 `;
-export const index = () => {
+export const Index = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    username: '',
+    father: '',
+    mother: '',
+    address: '',
+    dob: '',
+    phone: '',
+    department: '',
+    reg_no: '',
+    join_date: '',
+    qualified_exam: '',
+  });
+
+  const handleAddScholar = async () => {
+    const { data, error } = await supabase
+      .from('scholars_profiles')
+      .insert([formData])
+      .select();
+
+    if (data) {
+      alert(`User with name ${data[0].username} created successfully`);
+      router.push('/protected/office');
+    }
+    if (error) {
+      alert(`Some error occurred`);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -45,31 +75,86 @@ export const index = () => {
       <Container maxW='2xl' centerContent>
         <FormControl>
           <FormLabel>{`Full Name`}</FormLabel>
-          <Input type='Text' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+            type='Text'
+            mb={4}
+          />
           <FormLabel>{`Father's Name`}</FormLabel>
-          <Input type='Text' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, father: e.target.value })
+            }
+            type='Text'
+            mb={4}
+          />
           <FormLabel>{`Mother's Name`}</FormLabel>
-          <Input type='Text' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, mother: e.target.value })
+            }
+            type='Text'
+            mb={4}
+          />
           <FormLabel>{`Address`}</FormLabel>
-          <Input type='Text' />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+            type='Text'
+          />
           <FormLabel>{`D.O.B`}</FormLabel>
-          <Input type='date' mb={4} />
+          <Input
+            onChange={(e) => setFormData({ ...formData, dob: e.target.value })}
+            type='date'
+            mb={4}
+          />
           <FormLabel>{`Phone no.`}</FormLabel>
-          <Input type='number' mb={4} />
-          <FormLabel>{`Email`}</FormLabel>
-          <Input type='email' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, phone: e.target.value })
+            }
+            type='number'
+            mb={4}
+          />
           <FormLabel>{`Registration No.`}</FormLabel>
-          <Input type='Text' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, reg_no: e.target.value })
+            }
+            type='Text'
+            mb={4}
+          />
           <FormLabel>{`Joining Date`}</FormLabel>
-          <Input type='Date' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, join_date: e.target.value })
+            }
+            type='Date'
+            mb={4}
+          />
           <FormLabel>{`Qualified Exam`}</FormLabel>
-          <Input type='Text' mb={4} />
+          <Input
+            onChange={(e) =>
+              setFormData({ ...formData, qualified_exam: e.target.value })
+            }
+            type='Text'
+            mb={4}
+          />
         </FormControl>
-        <Button fontSize={26} colorScheme='teal' size='lg' width={'50%'}>
+        <Button
+          onClick={handleAddScholar}
+          fontSize={26}
+          colorScheme='teal'
+          size='lg'
+          width={'50%'}
+        >
           Submit
         </Button>
       </Container>
     </>
   );
 };
-export default index;
+export default Index;
