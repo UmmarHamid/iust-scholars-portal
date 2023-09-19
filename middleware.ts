445a003d19c7a9cf9862962730b652111f6507bc
Ummar Hamid <ummar@192.168.1.12>
2023-09-19 18:01:26 +0530

    restrict page access based on role

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { fetchUserDetails } from './utils/utils';
 
 export async function middleware(req: NextRequest) {
   // We need to create a response and hand it to the supabase client to be able to modify the response headers.
@@ -12,12 +13,50 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession();
 
+  const userDetails = await fetchUserDetails(session?.user.email || '');
+
   const redirectUrl = req.nextUrl.clone();
 
   // Check auth condition
   if (session?.user.email) {
-    // Authentication successful, forward request to protected route.
-    return res;
+    // User is authenticated
+
+    // Check user role
+    if (
+      userDetails.role === 'scholar' &&
+      req.nextUrl.pathname.startsWith('/protected/scholars')
+    ) {
+      return res;
+    }
+    if (
+      userDetails.role === 'supervisor' &&
+      req.nextUrl.pathname.startsWith('/protected/supervisor')
+    ) {
+      return res;
+    }
+    if (
+      userDetails.role === 'office-staff' &&
+      req.nextUrl.pathname.startsWith('/protected/office')
+    ) {
+      return res;
+    }
+    if (
+      userDetails.role === 'drc-staff' &&
+      req.nextUrl.pathname.startsWith('/protected/drc')
+    ) {
+      return res;
+    }
+    if (
+      userDetails.role === 'srac-staff' &&
+      req.nextUrl.pathname.startsWith('/protected/srac')
+    ) {
+      return res;
+    }
+
+    // If the user's role doesn't match the allowed routes, redirect to home page.
+    redirectUrl.pathname = '/';
+    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
   }
 
   // Auth condition not met, redirect to home page.
