e7532f4c88c14389655896622ef0e48db30cf12d
zubairahad <69452881+zubairahad@users.noreply.github.com>
2023-11-14 21:33:57 +0530

    add update-award

--- pages/protected/office/updateaward/index.tsx
+++ pages/protected/office/updateaward/index.tsx
@@ -3,21 +3,61 @@ import {
   Box,
   Button,
   Container,
+  FormControl,
   Heading,
   Input,
-  Stack,
+  SimpleGrid,
   Text,
+  Textarea,
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import BackIcon from '@/components/BackIcon/BackIcon';
-import Logout from '@/components/Logout/Logout';
+import { Select } from '@chakra-ui/react';
 import InnerFooter from '@/components/InnerFooter/InnerFooter';
-export const index = () => {
+import Logout from '@/components/Logout/Logout';
+import BackIcon from '@/components/BackIcon/BackIcon';
+import { GetServerSideProps } from 'next';
+import supabase from '@/utils/supabase';
+import { ScholarProfile } from '../scholardetails';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useState } from 'react';
+
+export const Index = ({ scholars }: any) => {
+  const [formData, setFormData] = useState({
+    viva_score: '',
+    credit_score: '',
+  });
+  const handleSubmitScore = async (scholarId: string) => {
+    const { data, error } = await supabase
+      .from('scholars_score')
+      .insert([formData])
+      .select();
+    const score = data ? data[0] : [];
+    console.log('score id is ...', score.id);
+    // const router = useRouter();
+    try {
+      // Make an update query to update the supervisor assignment in the Supabase table
+      const { error } = await supabase
+        .from('scholars_profiles') // Replace with your table name
+        .update({ credit_score: score.id })
+        .eq('id', scholarId);
+
+      if (error) {
+        throw error;
+      }
+
+      // Success! You can also update the local state if needed.
+      // For example, set a message to indicate success.
+      console.log(`Score assigned for scholar ${scholarId}`);
+    } catch (error) {
+      console.error('Error assigning score:', error);
+    }
+  };
   return (
     <>
       <Head>
         <title>Update Awards</title>
       </Head>
+
       <Box
         display={'flex'}
         justifyContent={'space-around'}
@@ -28,50 +68,70 @@ export const index = () => {
           <BackIcon />
         </Link>
         <Heading as={'h2'} color={'teal'} fontWeight={300}>
-          Update awards
+          Update Awards
         </Heading>
         <Logout />
       </Box>
-      <Container maxWidth={'5xl'} marginBottom={'26%'}>
-        <Box
-          display={'flex'}
-          justifyContent={'space-around'}
-          alignItems={'center'}
-        >
-          <Text fontSize={'2xl'} color={'#07443E'}>
-            1. Zubair DOCS IUST0121014521
-          </Text>
-          <Link href='/'>
-            <Stack direction={['row']} spacing={'2%'}>
-              <Input padding={'0 10px'} placeholder='Viva' size='xl' />
-              <Input padding={'0 10px'} placeholder='credit' size='xl' />
-              <Button colorScheme='blue' padding={'10px 30px'}>
+      <Container maxW='5xl' marginBottom={'20%'}>
+        {scholars.map((scholar: ScholarProfile, index: number) => (
+          <SimpleGrid key={scholar.id} columns={0} gap={0} mb={8}>
+            <Text fontSize={'2xl'} color={'#07443E'}>
+              {`${index + 1} - ${scholar.username} ${scholar.reg_no}`}
+            </Text>
+            {/* TODO: Add default value for the selectors */}
+            <SimpleGrid
+              marginTop={2}
+              display='flex'
+              flexDirection='row'
+              alignItems='center'
+              justifyContent='space-around'
+            >
+              <FormControl>
+                <Input
+                  variant='outline'
+                  padding={7}
+                  htmlSize={30}
+                  marginRight={4}
+                  width='auto'
+                  placeholder='Viva Score'
+                  onChange={(e) =>
+                    setFormData({ ...formData, viva_score: e.target.value })
+                  }
+                  mb={2}
+                />
+                <Input
+                  variant='outline'
+                  padding={7}
+                  marginRight={4}
+                  htmlSize={30}
+                  width='auto'
+                  placeholder='Credit Score'
+                  onChange={(e) =>
+                    setFormData({ ...formData, credit_score: e.target.value })
+                  }
+                  mb={2}
+                />
+              </FormControl>
+
+              <Button
+                size='lg'
+                colorScheme='blue'
+                onClick={() => handleSubmitScore(scholar.id)}
+              >
                 Submit
               </Button>
-            </Stack>
-          </Link>
-        </Box>
-        <Box
-          display={'flex'}
-          justifyContent={'space-around'}
-          alignItems={'center'}
-        >
-          <Text fontSize={'2xl'} marginTop={'2%'} color={'#07443E'}>
-            2. SUhail DOCS IUST0121014444
-          </Text>
-          <Link href='/'>
-            <Stack direction={['row']} spacing={'2%'}>
-              <Input padding={'0 10px'} placeholder='viva' size='xl' />
-              <Input padding={'0 10px'} placeholder='credit' size='xl' />
-              <Button padding={'10px 30px'} colorScheme='blue'>
-                Submit
-              </Button>
-            </Stack>
-          </Link>
-        </Box>
+            </SimpleGrid>
+          </SimpleGrid>
+        ))}
       </Container>
       <InnerFooter />
     </>
   );
 };
-export default index;
+export default Index;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data: scholars } = await supabase
+    .from('scholars_profiles')
+    .select('*');
+  return { props: { scholars } };
+};
